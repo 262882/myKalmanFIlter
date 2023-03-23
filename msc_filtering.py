f276bc1cd3daeda88699e34cb59c2598e5fdba08
262882 <262882@students.wits.ac.za>
2023-03-23 22:02:55 +0200

    Seperate classes for uni and multivariate

--- msc_filtering.py
+++ msc_filtering.py
@@ -1,7 +1,47 @@
 import numpy as np
 
-class MyKalmanFilter:
-    def __init__(self, xt_init, Pt_init, process_model, measure_noise, system_noise):
+class MyKalmanFilterUnivariate:
+    def __init__(self, xt_init: float, Pt_init: float, R: float, Q: float):
+        
+        #print("Initialise model")
+        
+        # State estimates
+        self.xt_prev = 0  # previous time period
+        self.xt_intr = 0  # intermediate time period
+        self.xt_curr = xt_init  # current time period
+        
+        # State covariances
+        self.Pt_prev = 0  # previous time period
+        self.Pt_intr = 0  # intermediate time period
+        self.Pt_curr = Pt_init  # current time period   
+        
+        # Noise covariances
+        self.r_noise = R  # Measurement noise
+        self.q_noise = Q  # System noise
+        
+        # Kalman gain
+        self.k_gain = 0
+    
+    def _predict(self):
+        #print("Predict")
+        self.xt_intr = self.xt_prev
+        self.Pt_intr = self.Pt_prev + self.q_noise
+        
+    def _correct(self, yt_measure):
+        #print("Update")
+        self.k_gain = self.Pt_intr/(self.Pt_intr+self.r_noise)
+        self.xt_curr = self.xt_intr + self.k_gain*(yt_measure-self.xt_intr)
+        self.Pt_curr = (1-self.k_gain)*self.Pt_intr
+    
+    def step(self, measurement: float):
+        #print("Step")
+        self.xt_prev = self.xt_curr
+        self.Pt_prev = self.Pt_curr
+        self._predict()
+        self._correct(measurement)
+
+class MyKalmanFilterMultivariate:
+    def __init__(self, xt_init: np.ndarray, Pt_init: np.ndarray, R: np.ndarray, Q: np.ndarray, F: np.ndarray, H: np.ndarray):
         
         #print("Initialise model")
         
@@ -16,30 +56,32 @@ class MyKalmanFilter:
         self.Pt_curr = Pt_init                 # current time period   
         
         # Noise covariances
-        self.r_noise = measure_noise  # Measurement noise
-        self.q_noise = system_noise  # System noise
+        self.r_noise = R  # Measurement noise
+        self.q_noise = Q  # System noise
         
         # Process model
-        self.model = process_model
+        self.F_trans = F  # State transition matrix
+        self.H_measure = H  # Measurement matrix
         
         # Kalman gain
         self.k_gain = np.empty_like(Pt_init)
     
     def _predict(self):
         #print("Predict")
-        self.xt_intr = self.model(self.xt_prev)
-        self.Pt_intr = self.Pt_prev + self.q_noise
+        self.xt_intr = self.F_trans@self.xt_prev
+        self.Pt_intr = self.F_trans@self.Pt_prev@self.F_trans.T + self.q_noise
         
     def _correct(self, yt_measure):
-        #print("Update")
-        self.k_gain = self.Pt_intr/(self.Pt_intr+self.r_noise)
-        self.xt_curr = self.xt_intr + self.k_gain*(yt_measure-self.xt_intr)
-        self.Pt_curr = (1-self.k_gain)*self.Pt_intr
+        #print("Correct")
+        self.k_gain = self.Pt_intr@self.H_measure.T@np.linalg.inv(self.H_measure@self.Pt_intr@self.H_measure.T+self.r_noise)
+        self.xt_curr = self.xt_intr + self.k_gain*(yt_measure-self.H_measure@self.xt_intr)
+        KH = self.k_gain@self.H_measure
+        self.Pt_curr = (np.eye(KH.shape())-KH)@self.Pt_intr
     
-    def step(self, measurement):
+    def step(self, measurement: np.ndarray):
         #print("Step")
-        self.xt_prev = self.xt_curr
-        self.Pt_prev = self.Pt_curr
+        self.xt_prev = np.copy(self.xt_curr)
+        self.Pt_prev = np.copy(self.Pt_curr)
         self._predict()
         self._correct(measurement)
         
